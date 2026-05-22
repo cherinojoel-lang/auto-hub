184515c97fa3863b8c61b0e13ef3f19398cc8625
Wix Remote Machine <support@wix.com>
2026-05-22 17:44:06 +0000

    Restored to checkpoint ec4d4e22090fc40e5d24621bc9eee7093e73de9c

---
+++ src/integrations/errorHandlers/ErrorPage.tsx
@@ -0,0 +1,20 @@
+import { useRouteError } from "react-router";
+import { useEffect, useRef } from "react";
+
+export default function ErrorPage() {
+  const ref = useRef<HTMLDivElement>(null);
+  const error = useRouteError() as Error;
+
+  useEffect(() => {
+    if (ref.current) {
+      const ErrorOverlay = window.customElements.get('vite-error-overlay');
+      if (ErrorOverlay) {
+        ref.current.appendChild(new ErrorOverlay(error, 'runtime'));
+      }
+    }
+  }, [error]);
+
+  return (
+    <div ref={ref}></div>
+  );
+}
